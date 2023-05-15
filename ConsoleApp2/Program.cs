445512c9551369d73caf245afd09b851bf2658ba
jamydewaal <35739@ma-web.nl>
2023-05-15 08:25:22 +0200

    enumsopdracht

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Colors myMainThemeColor = Colors.Purple;
+
+            if (myMainThemeColor == Colors.Purple)
+            {
+                Console.WriteLine("Dat is mijn kleur!");
+            }
+            else
+            {
+                Console.WriteLine("Dit is niet mijn kleur.");
+            }
+        }
+    }
+}
